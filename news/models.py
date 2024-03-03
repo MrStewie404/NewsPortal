da3fa4d56e2a43890fb53ac2bb6ce5ea770b76f2
Sai <chulkovdany1609@gmail.com>
2024-03-03 16:54:16 +0300

    main

---
+++ news/models.py
@@ -0,0 +1,133 @@
+# TODO Закончить работу над Post().create_post
+# Ошибка: ValueError: Cannot assign "1": "Post.author" must be a "Author" instance.
+
+from django.db import models
+import time
+
+news = 'NE'
+article = 'AR'
+CONTENT_TYPE = [
+    (news, 'Новость'),
+    (article, 'Статья')
+]
+
+class User(models.Model):
+    id = models.IntegerField(primary_key=True)
+    first_name = models.TextField()
+    last_name = models.TextField()
+    username = models.TextField()
+    date_registration = models.DateField()
+
+    def create_user(self, first_name, last_name, username):
+        user = User(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            date_registration=time.strftime('%Y-%m-%d')
+        )
+        user.save()
+
+class Author(models.Model):
+    user = models.OneToOneField("User", on_delete = models.CASCADE)
+    rating = models.FloatField()
+    def update_rating(self):
+        # rating = Author()
+        # save.rating()
+        # return self.rating + 1
+        ...
+    
+    def create_author(self, id):
+        author = Author(rating = 0, user_id = id)
+        author.save()
+
+class Category(models.Model):
+    name = models.TextField(unique = True)
+
+    def create_category(self, name):
+        category = Category(name = name)
+        category.save()
+
+class Post(models.Model):
+    content_type = models.CharField(max_length = 2,
+                                  choices = CONTENT_TYPE,
+                                  default = article)
+    author = models.ForeignKey("Author", on_delete = models.CASCADE)
+    datetime = models.DateTimeField()
+    category = models.ManyToManyField("Category")
+    title = models.TextField()
+    text = models.TextField()
+    rating = models.FloatField()
+
+    def set_now_time(self):
+        return time.strftime('%Y-%m-%d %H:%M:%S')
+
+    def create_post(self, content_type: str = 'Новость | Статья', 
+                    author: int = 2, 
+                    category: str = 'Игры | Музыка | Интернет | Искусство', 
+                    title: str = "...", 
+                    text: str = "..."):
+        post = Post(content_type = content_type,
+                    author = author,
+                    datetime = Post().set_now_time(),
+                    category = category,
+                    title = title,
+                    text = text,
+                    rating = 0)
+        post.save()
+
+    def like(self, id):
+        rating = Post.rating()
+        rating.save()
+
+    def dislike(self):
+        return self.rating - 1
+
+    def preview(self, id):
+        post = Post.objects.filter(id = id)
+        return f"{post.values('text')[:124]}..."
+
+class PostCategory(models.Model):
+    post = models.ForeignKey("Post", on_delete = models.CASCADE)
+    category = models.ForeignKey("Category", on_delete = models.CASCADE)
+
+class Comment(models.Model):
+    post = models.ForeignKey("Post", on_delete = models.CASCADE)
+    user = models.ForeignKey("User", on_delete = models.CASCADE)
+    text = models.TextField()
+    datetime = models.DateTimeField()
+    rating = models.IntegerField()
+
+    def set_now_time(self):
+        return time.strftime('%Y-%m-%d %H:%M:%S')
+
+    rating = models.FloatField()
+
+    def like(self):
+        rating = Comment.rating + 1
+        rating.save()
+
+    def dislike(self):
+        rating = Comment.rating - 1
+        rating.save()
+
+if __name__ == '__main__':
+    Post().create_post(content_type = 'Новость', author = 1, category = 'Игры',
+                       title = 'Thorin составил свой список лучших игроков в CS c 2012 по 2023-й', 
+text = """
+Киберспортивный журналист Данкан Thorin Шилдс назвал лучших игроков в CS:GO по годам. Свою версию топа британец представил в X (бывшая Twitter). Единственным расхождением с вариантом от HLTV.org оказался выбор в 2020-м — Шилдс назвал лучшим в этом сезоне Александра s1mple Костылева, а не Матье ZywOo Эрбо.
+
+Данкан Thorin Шилдс
+Киберспортивный журналист
+2012 — GeT_RiGhT
+2013 — GeT_RiGhT
+2014 — GeT_RiGhT
+2015 — olofmeister
+2016 — coldzera
+2017 — coldzera
+2018 — s1mple
+2019 — ZywOo
+2020 — s1mple
+2021 — s1mple
+2022 — s1mple
+2023 — ZywOo
+""")
