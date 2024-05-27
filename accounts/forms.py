f3dbf68bd69e31dc14d17b87ea0f46be08fc071b
MrStewie404 <chulkovdany1609@gmail.com>
2024-05-27 18:09:34 +0300

    add email

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from allauth.account.forms import SignupForm
 from django.contrib.auth.models import Group
+from django.core.mail import EmailMultiAlternatives
+from django.core.mail import send_mail
+from django.core.mail import mail_managers
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="Email")
@@ -19,11 +22,29 @@ class SignUpForm(UserCreationForm):
             "password1",
             "password2",
         )
-
+    
 
 class CustomSignupForm(SignupForm):
     def save(self, request):
         user = super().save(request)
         authors = Group.objects.get(name="authors")
         user.groups.add(authors)
+
+        subject = 'Добро пожаловать в актуальный поток информации!'
+        text = f'{user.username}, вы успешно зарегистрировались на сайте!'
+        html = (
+            f'<b>{user.username}</b>, вы успешно зарегистрировались на '
+            f'<a href="http://127.0.0.1:8000/products">сайте</a>!'
+        )
+        msg = EmailMultiAlternatives(
+            subject=subject, body=text, from_email=None, to=[user.email]
+        )
+        msg.attach_alternative(html, "text/html")
+        msg.send()
+
+        mail_managers(
+            subject='Новый пользователь!',
+            message=f'Пользователь {user.username} зарегистрировался на сайте.'
+        )
+
         return user
