e95c6469ebe015d4944ebae1f6fbc27ff7083d23
Sai <chulkovdany1609@gmail.com>
2024-05-28 21:18:48 +0300

    error

--- news/signals.py
+++ news/signals.py
@@ -30,38 +30,13 @@ def send_notification(preview, pk, title, subscribers):
 
 
 @receiver(m2m_changed, sender=Category)
-def Post_created(instance, created, **kwargs):
+def Post_created(instance, **kwargs):
     if kwargs['action'] == 'post_add':
         categories = instance.category.all()
         subscribers = list[str] = []
         for category in categories:
             subscribers += category.subscribers.all()
 
-
         subscribers = [s.email for s in subscribers]
 
         send_notification(instance.preview(), instance.pk, instance.title, subscribers)
-    # if not created:
-    #     return
-
-    # emails = User.objects.filter(
-    #     subscriptions__category=instance.category
-    # ).values_list('email', flat=True)
-
-    # subject = f'Новый пост в категории {instance.category} !'
-
-    # text_content = (
-    #     f'Название: {instance.title}\n'
-    #     f'Дата: {instance.datetime}\n\n'
-    #     f'Перейти...: http://127.0.0.1:8000{instance.get_absolute_url()}'
-    # )
-    # html_content = (
-    #     f'Название: {instance.title}<br>'
-    #     f'Дата: {instance.datetime}<br><br>'
-    #     f'<a href="http://127.0.0.1{instance.get_absolute_url()}">'
-    #     f'Перейти...</a>'
-    # )
-    # for email in emails:
-    #     msg = EmailMultiAlternatives(subject, text_content, None, [email])
-    #     msg.attach_alternative(html_content, "text/html")
-    #     msg.send()
